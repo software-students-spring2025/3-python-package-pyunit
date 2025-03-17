d7d2b99621ee981fca8a4e421a44944b5222689d
MahmoudS1201 <mss9253@nyu.edu>
2025-03-17 14:50:08 -0400

    Added all neccesary structure and files for the initial commit. Created the lazy_commit_message function and 3 tests for it

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lazydev",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Mahmoud Shehata, Syed Naqvi, Brandon___, David Yu",
+    author_email="mss9253@nyu.edu, ____, ____, ____,",
+    description="A package for lazy developers so that they can write commit messages, write pull request titles, generate random excuses, and other assorted laziness without thinking",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/software-students-spring2025/3-python-package-pyunit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
