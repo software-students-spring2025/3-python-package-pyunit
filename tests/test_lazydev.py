aec9a2a461bc2406b8e140f3c11189ec8a5e9238
bamoeq <brandonmorales1226@gmail.com>
2025-03-18 13:50:58 -0400

    Escuse for no test cases

--- tests/test_lazydev.py
+++ tests/test_lazydev.py
@@ -1,5 +1,5 @@
 import pytest
-from lazydev.lazydev import lazy_commit_message
+from lazydev.lazydev import lazy_commit_message, lazy_test_excuse
 
 #Tests for lazy_commit_message
 #Ensures Keyword is in the commit message when added
@@ -21,3 +21,20 @@ def test_commit_message_random():
     message1 = lazy_commit_message("keyword")
     message2 = lazy_commit_message("keyword")
     assert message1 != message2, "messages should be different each time"
+
+#test if the function returns string 
+def test_excuse_is_not_empty():
+    excuse=lazy_test_excuse()
+    assert isinstance(excuse,str),"Excuse should be string"
+
+#checks if the reason input is embedded 
+def test_excuse_reason():
+    reason="My dog ate my code"
+    excuse=lazy_test_excuse(reason)
+    assert reason in excuse, f"'{reason} should be in the {excuse}'"
+# This is to make sure the reason and excuse is grammatically in checl
+def test_structured_sentence():
+    excuse=lazy_test_excuse()
+    assert excuse,"Excuse should not be empty"
+    assert excuse[0].isupper(),f"Excuse should start with a capital letter: {excuse}"
+    assert excuse[-1] in {".", "!"}
